f5aa7b9f1e62097bd0c82e3154d6c39076e32216
Rob Bebbington <rob.bebbington@outlook.com>
2025-06-21 21:38:28 +1000

    Draw a dashboard for 3_Stock_Chart complete

--- US_market/pages/3_Stock_Chart.py
+++ US_market/pages/3_Stock_Chart.py
@@ -3,7 +3,11 @@ from datetime import date, datetime
 import altair as alt
 import polars as pl
 from securities_load.securities.polar_table_functions import (
+    retrieve_exchange_ids_using_country_alpha_3,
     retrieve_ohlcv_using_ticker_id_and_dates,
+    retrieve_ticker_types,
+    retrieve_tickers_using_exchanges_and_ticker_types,
+    retrieve_unique_country_alpha_3_from_exchanges,
 )
 
 import streamlit as st
@@ -11,43 +15,58 @@ import streamlit as st
 st.set_page_config(
     page_title="Stock Chart",
     layout="wide",
-    # page_icon="👋",
 )
 
 st.title("Stock Chart")
 
-# watchlist_data = retrieve_watchlists_using_watchlist_type("Dashboard")
-# watchlist_ids = [x[0] for x in watchlist_data]
-# watchlist_codes = [x[1] for x in watchlist_data]
-# watchlist_description = [x[2] for x in watchlist_data]
+countries = retrieve_unique_country_alpha_3_from_exchanges()
+country_codes = countries["country_alpha_3"].to_list()
+ticker_types = retrieve_ticker_types()
+ticker_type_ids = ticker_types["id"].to_list()
+ticker_type_codes = ticker_types["code"].to_list()
 
 col1, col2 = st.columns([0.4, 0.6])
 
-# with col1:
-#     watchlist = st.pills(
-#         "Select a watchlist to get symbols:",
-#         watchlist_codes,
-#         selection_mode="single",
-#         default=watchlist_codes[1],
-#     )
+with col1:
+    country = st.pills(
+        "Select a country to get symbols:",
+        country_codes,
+        selection_mode="single",
+        default=country_codes[2],
+    )
 
-# if watchlist:
-#     ticker_data = retrieve_tickers_using_watchlist_code(watchlist)
-#     ticker_ids = [x[0] for x in ticker_data]
-#     ticker_codes = [x[1] for x in ticker_data]
-#     ticker_names = [x[2] for x in ticker_data]
+with col2:
+    ticker_type = st.pills(
+        "Select a ticker type to get symbols:",
+        ticker_type_codes,
+        selection_mode="single",
+        default=ticker_type_codes[6],
+    )
 
-# with col2:
-#     ticker_code = st.pills(
-#         "Select a ticker to chart:",
-#         ticker_codes,
-#         selection_mode="single",
-#         default=ticker_codes[0],
-#     )
+    if ticker_type:
+        ticker_type_index = ticker_type_codes.index(ticker_type)
+        ticker_type_id = ticker_type_ids[ticker_type_index]
 
-# if ticker_code:
-#     ticker_index = ticker_codes.index(ticker_code)
-#     ticker_name = ticker_names[ticker_index]
+if country:
+    exchanges = retrieve_exchange_ids_using_country_alpha_3(country)
+    exchange_ids = exchanges["id"].to_list()
+
+tickers = retrieve_tickers_using_exchanges_and_ticker_types(
+    exchange_ids, ticker_type_id
+)
+ticker_ids = tickers["id"].to_list()
+ticker_symbols = tickers["ticker"].to_list()
+ticker_names = tickers["name"].to_list()
+
+with col1:
+    ticker = st.selectbox(
+        "Select a symbol:",
+        ticker_symbols,
+    )
+    if ticker:
+        ticker_index = ticker_symbols.index(ticker)
+        ticker_id = ticker_ids[ticker_index]
+        ticker_name = ticker_names[ticker_index]
 
 today = datetime.now()
 last_year = date(today.year - 1, 1, 1)
@@ -75,16 +94,38 @@ else:
 
 averages = ["5", "10", "20", "50", "200"]
 
-with col2:
-    moving_averages = st.pills(
-        "Select one or more moving average periods to display:",
-        averages,
-        selection_mode="multi",
-        default=averages[3:5],
-    )
+moving_averages = []
 
-ticker_id = 148055
-ticker_code = "ZXZX"
+with col2:
+    st.write("Specify periods for moving averages. Set to zero if not required.")
+    col2a, col2b, col2c, col2d = st.columns([0.25, 0.25, 0.25, 0.25])
+    with col2a:
+        ma1 = st.number_input(
+            ":blue[Simple moving average 1]",
+            value=None,
+            step=1,
+            key="ma1",
+        )
+        if ma1:
+            moving_averages.append(ma1)
+    with col2b:
+        ma2 = st.number_input(
+            ":orange[Simple moving average 2]", value=None, step=1, key="ma2"
+        )
+        if ma2:
+            moving_averages.append(ma2)
+    with col2c:
+        ma3 = st.number_input(
+            ":red[Simple moving average 3]", value=None, step=1, key="ma3"
+        )
+        if ma3:
+            moving_averages.append(ma3)
+    with col2d:
+        ma4 = st.number_input(
+            ":violet[Simple moving average 4]", value=None, step=1, key="ma4"
+        )
+        if ma4:
+            moving_averages.append(ma4)
 
 
 @st.cache_data
@@ -96,15 +137,13 @@ def load_data(ticker_id, start, end):
 
 data = load_data(ticker_id, start, end)
 data = data.with_columns(pl.col("date").dt.strftime("%Y-%m-%d").alias("date_as_string"))
-# st.write(data)
 ## Simple Moving Average
 for ave in moving_averages:
-    ave_num = int(ave)
-    ave_name = "MA-" + ave
+    ave_name = "MA-" + str(ave)
     data = data.with_columns(
-        pl.col("close").rolling_mean(window_size=ave_num).alias(ave_name)
+        pl.col("close").rolling_mean(window_size=ave).alias(ave_name)
     )
-MA_colors = ["#170c3b", "#fa8d0b", "#7a1d8d", "#e4eb27", "#2de3e6"]
+MA_colors = ["blue", "orange", "red", "violet"]
 base = alt.Chart(data).encode(
     x=alt.X(
         "date_as_string:O",
@@ -136,23 +175,15 @@ bar = (
         ),
         tooltip=["date:T", "open", "high", "low", "close"],
     )
-    .properties(width=600, height=400, title="Candlestick")
+    .properties(width=600, height=400, title=f"Candlestick for {ticker_name}")
 )
 
 candlestick = rule + bar
 
-# sma = base.mark_line(color="purple").encode(
-#     y=alt.Y("MA-10:Q"),
-#     tooltip=["date:T", "MA-10:Q"],
-# )
-# candlestick = rule + bar + sma
-
-
 # Simple Moving Average
 color_count = 0
 for ave in moving_averages:
-    ave_num = int(ave)
-    ave_name = "MA-" + ave
+    ave_name = "MA-" + str(ave)
     sma = base.mark_line(color=MA_colors[color_count]).encode(
         y=alt.Y(ave_name + ":Q"),
         tooltip=["date", ave_name + ":Q"],
